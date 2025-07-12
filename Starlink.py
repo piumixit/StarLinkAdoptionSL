ef35b05a3093fb214cbd0274867cd264b697d7f3
piumixit <piumixit@gmail.com>
2025-07-12 18:53:05 +0530

    Update Starlink.py

--- Starlink.py
+++ Starlink.py
@@ -9,9 +9,6 @@ st.set_page_config(
 st.title("Starlink Adoption Prediction in Sri Lanka")
 st.write("This application provides an interactive exploration of the Starlink adoption prediction model for Sri Lanka.")
 
-st.sidebar.success("Select a page above.")
-
-# Add any introductory content or instructions for the main page here.
 st.markdown(
     """
     **Objective:** To build a machine‑learning model to predict the likelihood of Starlink internet adoption across Sri Lanka’s districts.
