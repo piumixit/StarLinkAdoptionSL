eadc3fe69e1c66677a3bb3295e07c61d6d200f57
piumixit <piumixit@gmail.com>
2025-07-12 19:52:45 +0530

    Update EDA.py

--- pages/EDA.py
+++ pages/EDA.py
@@ -28,7 +28,7 @@ def run_eda_page():
     #st.text(info_str)
 
     st.subheader("Data")
-    st.dataframe(df1.head())
+    st.dataframe(df1.describe())
 
     st.subheader("Numeric Summary")
     st.dataframe(df1.describe().T)
